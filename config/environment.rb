dff7f8b57fb63e53262bc9a5b03471fd9d48a429
Jacob Wylie <jacobwylie@gmail.com>
2016-12-31 16:06:46 +0000

    Added mailer settings to environment

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => ENV'sleepy-depths-30114.herokuapp.com',
-  :authentication => ENV:plain,
+  :domain         => 'sleepy-depths-30114.herokuapp.com',
+  :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
